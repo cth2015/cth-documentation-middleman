e670650bccdff76bfe478536f831bdc55560e6cf
Zaturrby <rjkorteschiel@gmail.com>
2015-12-31 12:18:23 +0100

    add prefix

--- config.rb
+++ config.rb
@@ -41,6 +41,8 @@ configure :build do
 
   # Minify Javascript on build
   # activate :minify_javascript
+
+  :http_prefix = "/cth-documentation-middleman/"
 end
 
 # Activate the blog gems for both the story and the articles

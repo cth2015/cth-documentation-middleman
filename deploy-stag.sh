467035c8198f25517b8f41ae5e13a06c80527455
Zaturrby <rjkorteschiel@gmail.com>
2016-02-14 16:26:51 +0100

    little change

--- deploy-stag.sh
+++
@@ -1,8 +0,0 @@
-bundle exec middleman build
-cd build
-git init
-git remote add origin git@github.com:cth2015/cth-documentation-middleman-staging.git
-git add -A
-git commit -m "deploy"
-git checkout -B gh-pages
-git push -u origin gh-pages -f
